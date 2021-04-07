82a48ebe4ecaae743e99bdc55a75b42c07e35b4a
brigittasz <48497331+brigittasz@users.noreply.github.com>
2021-04-07 19:10:07 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from world import *
+
+def printWorldValues(world):
+    #loop across all locations
+    for row in world.values:
+        line = ''
+        for e in row:
+            line += '%3.3f ' % e #build a string of each line
+        print(line) #print string
+
+def printWorldPolicy(world):
+      for row in world.policy:
+        line = ''
+        for e in row:
+            line += e + ' '
+        print(line)
+
+
+if __name__ == "__main__":
+    #worldName = input('World file name: ')
+    #i = int(input('Number of iterations: '))
+    w = World("sample.json")
+    #w.valueIteration(i)
+    #printWorldValues(w)
+    #printWorldPolicy(w)
